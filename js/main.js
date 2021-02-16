0675e5fc9e3019a4e4e019c6e7891692c2e903c7
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2021-02-16 13:33:35 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var number = 5;
+var string = 'Hello!';
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4 / 0);
+console.log('string' * 9);
+
+
+let something;
+console.log(something);
+
+let persone = {
+  name: 'John',
+  age: 28,
+  isMarried: false,
+};
+
+console.log(persone['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0]);
+
+// // alert('Hello World!');
+
+// let answer = +prompt('Есть ли вам 18?', "да");
+
+// console.log(typeof (answer));
+
+// console.log('arr' + ' - object');
+// console.log(4 + ' - object');
+
+let incr = 10;
+let decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log("2" === 2);
+
+let isChecked = true;
+let isClose = false;
+console.log(isChecked && isClose);

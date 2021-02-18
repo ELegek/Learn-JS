59082a877b214ba26ea17dfd9a31b2a2cc8b89d4
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2021-02-18 20:39:27 +0300

    3 task

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+// Типы данных
 var number = 5;
 var string = 'Hello!';
 var sym = Symbol();
@@ -8,12 +10,12 @@ null;
 undefined;
 var obj = {};
 
-console.log(4 / 0);
-console.log('string' * 9);
+// console.log(4 / 0);
+// console.log('string' * 9);
 
 
-let something;
-console.log(something);
+// let something;
+// console.log(something);
 
 let persone = {
   name: 'John',
@@ -21,7 +23,7 @@ let persone = {
   isMarried: false,
 };
 
-console.log(persone['name']);
+// console.log(persone['name']);
 
 let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
 
@@ -36,17 +38,77 @@ let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
 // console.log('arr' + ' - object');
 // console.log(4 + ' - object');
 
-let incr = 10;
-let decr = 10;
+// let incr = 10;
+// let decr = 10;
 
 
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-console.log(5 % 2);
-console.log("2" === 2);
+// console.log(5 % 2);
+// console.log("2" === 2);
 
-let isChecked = true;
-let isClose = false;
-console.log(isChecked && isClose);
+// let isChecked = true;
+// let isClose = false;
+// console.log(isChecked && isClose);
+
+
+// Условия
+
+let num = 50;
+
+if (num < 49) {
+  console.log('Неверно!');
+} else if (num > 100) {
+  console.log('Многовато!');
+} else {
+  console.log('Верно!');
+};
+
+(num == 50) ? console.log('Верно!'): console.log('Неверно!');
+
+
+switch (num) {
+  case num < 49:
+    console.log('Неверно!');
+    break;
+  case num > 100:
+    console.log('Многовато!');
+    break;
+  case num > 80:
+    console.log('Все еще многовато!');
+    break;
+  case 50: //Правильно записываем значение
+    console.log('Верно!');
+    break; //Всегда необходимо ставить после каждого действия
+  default:
+    console.log('Что-то пошло не так!');
+    break;
+}
+
+
+// Циклы
+
+
+// while (num < 55) {
+//   console.log(num);
+//   num++
+// }
+
+do {
+  console.log(num);
+  num++
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+  if (i == 6) {
+    continue;
+  }
+  console.log(i);
+}
+
+
+
+console.log(2 && 1 && null && 0 && undefined);

bf7fbae3aae8abaa6426f97a60b301b915bc7081
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2021-02-20 11:36:57 +0300

    task 4

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 
 
 // Типы данных
-var number = 5;
-var string = 'Hello!';
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
+// var number = 5;
+// var string = 'Hello!';
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
 
 // console.log(4 / 0);
 // console.log('string' * 9);
@@ -17,15 +17,15 @@ var obj = {};
 // let something;
 // console.log(something);
 
-let persone = {
-  name: 'John',
-  age: 28,
-  isMarried: false,
-};
+// let persone = {
+//   name: 'John',
+//   age: 28,
+//   isMarried: false,
+// };
 
 // console.log(persone['name']);
 
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
 
 // console.log(arr[0]);
 
@@ -56,36 +56,36 @@ let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
 
 // Условия
 
-let num = 50;
+// let num = 50;
 
-if (num < 49) {
-  console.log('Неверно!');
-} else if (num > 100) {
-  console.log('Многовато!');
-} else {
-  console.log('Верно!');
-};
+// if (num < 49) {
+//   console.log('Неверно!');
+// } else if (num > 100) {
+//   console.log('Многовато!');
+// } else {
+//   console.log('Верно!');
+// };
 
-(num == 50) ? console.log('Верно!'): console.log('Неверно!');
+// (num == 50) ? console.log('Верно!'): console.log('Неверно!');
 
 
-switch (num) {
-  case num < 49:
-    console.log('Неверно!');
-    break;
-  case num > 100:
-    console.log('Многовато!');
-    break;
-  case num > 80:
-    console.log('Все еще многовато!');
-    break;
-  case 50: //Правильно записываем значение
-    console.log('Верно!');
-    break; //Всегда необходимо ставить после каждого действия
-  default:
-    console.log('Что-то пошло не так!');
-    break;
-}
+// switch (num) {
+//   case num < 49:
+//     console.log('Неверно!');
+//     break;
+//   case num > 100:
+//     console.log('Многовато!');
+//     break;
+//   case num > 80:
+//     console.log('Все еще многовато!');
+//     break;
+//   case 50: //Правильно записываем значение
+//     console.log('Верно!');
+//     break; //Всегда необходимо ставить после каждого действия
+//   default:
+//     console.log('Что-то пошло не так!');
+//     break;
+// }
 
 
 // Циклы
@@ -96,19 +96,59 @@ switch (num) {
 //   num++
 // }
 
-do {
-  console.log(num);
-  num++
+// do {
+//   console.log(num);
+//   num++
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//   if (i == 6) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// Функции 
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//   alert(text);
+//   num = 10;
+// }
+
+// showFirstMessage('Hello World!');
+// console.log(num);
+
+// let calc = function(a, b) {
+//   return (a + b);
+// }
+
+let calc = (a, b) => a + b
+
+console.log(calc(3, 4));
+console.log(calc(8, 4));
+
+function retVar() {
+  let num = 50;
+  return num;
 }
-while (num < 55);
 
-for (let i = 1; i < 8; i++) {
-  if (i == 6) {
-    continue;
-  }
-  console.log(i);
-}
+let anotherNum = retVar();
+console.log(anotherNum);
 
+let str = 'test';
+console.log(str.length);
 
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
 
-console.log(2 && 1 && null && 0 && undefined);
+let twelve = '12.2px'
+// console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));

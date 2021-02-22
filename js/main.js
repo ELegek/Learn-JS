e1bd4ad8eef611371ee05c496bb6c717d513a000
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2021-02-22 13:50:33 +0300

    task 5

--- js/main.js
+++ js/main.js
@@ -129,26 +129,126 @@
 //   return (a + b);
 // }
 
-let calc = (a, b) => a + b
+// let calc = (a, b) => a + b
 
-console.log(calc(3, 4));
-console.log(calc(8, 4));
+// console.log(calc(3, 4));
+// console.log(calc(8, 4));
 
-function retVar() {
-  let num = 50;
-  return num;
+// function retVar() {
+//   let num = 50;
+//   return num;
+// }
+
+// let anotherNum = retVar();
+// console.log(anotherNum);
+
+// let str = 'test';
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let twelve = '12.2px'
+// console.log(Math.round(twelve));
+// console.log(parseInt(twelve));
+// console.log(parseFloat(twelve));
+
+// function first() {
+//   setTimeout(function () {
+//     console.log(1);
+//   }, 500);
+// }
+
+// function second() {
+//   console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//   console.log('Я учу ' + lang);
+//   callback();
+// }
+
+// function done() {
+//   console.log('Я прошел 3й урок');
+// }
+
+// learnJS('JavaScript', done);
+
+// let options = {
+//   width: 1024,
+//   height: 1024,
+//   name: 'test'
+// };
+
+// console.log(options.name);
+// options.bool = false;
+// options.colors = {
+//   border: 'black',
+//   bg: 'red'
+// };
+
+// delete options.bool;
+
+// console.log(options);
+
+// for (let key in options) {
+//   console.log('Свойства ' + key + ' имеет значение ' + options[key]);
+// }
+
+// console.log(Object.keys(options).length);
+
+// let arr = ['first', 2, 3, 'four', 5];
+
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// arr.forEach(function (item, i, mass) {
+//   console.log(i + ': ' + item + ' (массив: ' + mass + ')');
+// });
+
+// console.log(arr);
+
+// let mass =[1,3,4,6,7];
+
+// for (let key of mass) {
+//   console.log(key);
+// }
+
+// let ans = prompt('', ''),
+//   arr = [];
+
+// arr = ans.split(',');
+// console.log(arr);
+
+// let arr = ['aawe', 'zzz', 'pp', 'rqa'],
+//   i = arr.join();
+
+// console.log(i);
+
+let arr = [1, 15, 4],
+  i = arr.sort();
+
+function compareNum(a, b) {
+  return a - b;
 }
 
-let anotherNum = retVar();
-console.log(anotherNum);
+console.log(arr);
 
-let str = 'test';
-console.log(str.length);
+let soldier = {
+  health: 400,
+  armor: 100
+}
 
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
+let john = {
+  health: 100
+}
 
-let twelve = '12.2px'
-// console.log(Math.round(twelve));
-console.log(parseInt(twelve));
-console.log(parseFloat(twelve));
+john.__proto__ = soldier;
+
+console.log(john);
+console.log(john.armor);

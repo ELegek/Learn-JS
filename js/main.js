171e0abb810004c111379923090d0656f93cca48
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2021-03-10 23:02:35 +0300

    task 8

--- js/main.js
+++ js/main.js
@@ -319,3 +319,34 @@
 // let yourOpinion = prompt('Как вы относитесь к технике Apple?');
 
 // promptforApple.textContent = yourOpinion;
+
+let btn = document.querySelectorAll('button'),
+    wrap = document.querySelector('.wrapper'),
+    link = document.querySelector('a');
+
+// btn[0].onclick = function () {
+//     alert('Вы нажали первую кноаку!');
+// };
+
+// btn[0].onclick = function () {
+//     alert('Вы опять нажали нажали первую кноаку!');
+// };
+
+btn[0].addEventListener('click', function () {
+    console.log('Произошло событие! ' + event.type + 'на элементе ' + event.target);
+});
+
+wrap.addEventListener('click', function () {
+    console.log('Произошло событие! ' + event.type + 'на элементе ' + event.target);
+});
+
+link.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('Произошло событие! ' + event.type + 'на элементе ' + event.target);
+});
+
+btn.forEach(function (item) {
+    item.addEventListener('mouseleave', function () {
+        console.log('Вышли!');
+    });
+});
